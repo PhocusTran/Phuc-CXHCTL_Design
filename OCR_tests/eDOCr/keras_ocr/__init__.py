db8b695e9531d9a12c3a3717337b728e8758312f
PhocusTran <166520998+PhocusTran@users.noreply.github.com>
2025-05-17 14:06:19 +0700

    Delete OCR_tests directory

--- OCR_tests/eDOCr/keras_ocr/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import tools
-from . import detection
-from . import recognition
-from . import data_generation
-from . import evaluation
-from . import datasets
-from . import config
-
-__version__ = "0.9.0"
