ab41fc4f80c7eedd1d7418bb9e205d35bb5351e7
PhocusTran <166520998+PhocusTran@users.noreply.github.com>
2025-05-17 14:06:59 +0700

    Delete USB_WiFi directory

--- USB_WiFi/Raspberry/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField('Tên đăng nhập', validators=[DataRequired()])
-    password = PasswordField('Mật khẩu', validators=[DataRequired()])
-    submit = SubmitField('Đăng nhập')
